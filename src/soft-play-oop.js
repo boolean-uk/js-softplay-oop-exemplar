a8ade7369d9385d130d4f662ab9525a9f8074c98
phillipdunn <phillipdunn@hotmail.com>
2022-06-08 16:23:58 +0100

    add solution

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,38 @@
 // TODO: Write your class in this file
 
 // TODO: Change undefined to the name of your class
-module.exports = {
-  SoftPlay: undefined
+
+class SoftPlay {
+  constructor() {
+    this.adults = 0;
+    this.children = 0;
+  }
+
+  occupancy() {
+    return { adults: this.adults, children: this.children };
+  }
+
+  enter(numAdults, numChildren) {
+    // eslint-disable-next-line prettier/prettier
+    if (this.children > this.adults || numAdults < 1) {
+      return false;
+    }
+    this.adults += numAdults;
+    this.children += numChildren;
+    return true;
+  }
+
+  leave(numAdults, numChildren) {
+    // eslint-disable-next-line prettier/prettier
+    if (this.children - numChildren > this.adults - numAdults || numChildren > this.children || numAdults < 1) {
+      return false;
+    }
+    this.adults -= numAdults;
+    this.children -= numChildren;
+    return true;
+  }
 }
+
+module.exports = {
+  SoftPlay,
+};
