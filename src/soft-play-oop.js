92552692ed160b56462056f1606aa70a3fe8771d
phillipdunn <phillipdunn@hotmail.com>
2022-06-08 16:27:11 +0100

    update remove comments

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,38 +1,32 @@
-// TODO: Write your class in this file
-
-// TODO: Change undefined to the name of your class
-
 class SoftPlay {
-  constructor() {
-    this.adults = 0;
-    this.children = 0;
+  constructor () {
+    this.adults = 0
+    this.children = 0
   }
 
-  occupancy() {
-    return { adults: this.adults, children: this.children };
+  occupancy () {
+    return { adults: this.adults, children: this.children }
   }
 
-  enter(numAdults, numChildren) {
-    // eslint-disable-next-line prettier/prettier
+  enter (numAdults, numChildren) {
     if (this.children > this.adults || numAdults < 1) {
-      return false;
+      return false
     }
-    this.adults += numAdults;
-    this.children += numChildren;
-    return true;
+    this.adults += numAdults
+    this.children += numChildren
+    return true
   }
 
-  leave(numAdults, numChildren) {
-    // eslint-disable-next-line prettier/prettier
+  leave (numAdults, numChildren) {
     if (this.children - numChildren > this.adults - numAdults || numChildren > this.children || numAdults < 1) {
-      return false;
+      return false
     }
-    this.adults -= numAdults;
-    this.children -= numChildren;
-    return true;
+    this.adults -= numAdults
+    this.children -= numChildren
+    return true
   }
 }
 
 module.exports = {
-  SoftPlay,
-};
+  SoftPlay
+}
